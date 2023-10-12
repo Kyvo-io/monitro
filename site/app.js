9b4a58433f654837bf7b766e802895edac346030
Leandro Coelho Saraiva <leandrocoelhosaraiva@outlook.com>
2023-10-11 21:09:59 -0300

    Fix na rota inicial

--- site/app.js
+++ site/app.js
@@ -27,7 +27,7 @@ function iniciarServidor() {
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
-    app.use(express.static(path.join(__dirname, "public")));
+    app.use(express.static(path.join(__dirname, "public/institucional")));
 
     app.use(cors());
 
