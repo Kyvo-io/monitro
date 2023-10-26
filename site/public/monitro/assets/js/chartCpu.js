0203e5c28bccc5de5611a3c89cbb1d91f16b8017
carlosgoes0 <carlos.goes@sptech.school>
2023-10-26 16:04:10 -0300

    Ajuste ChartJs

--- site/public/monitro/assets/js/chartCpu.js
+++ site/public/monitro/assets/js/chartCpu.js
@@ -1,17 +1,46 @@
 const monitoramentoCPU = document.getElementById('monitoramentoCPU');
 
 new Chart(monitoramentoCPU, {
-  type: 'line',
+  type: 'bar',
   data: {
-    labels: ['Red', 'Blue'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19],
-      borderWidth: 1
-    }]
+    labels: [''],
+    datasets: [
+      {
+        label: 'Uso Atual',
+        data: [80],
+        backgroundColor: '#ff090993'
+      },
+      {
+        label: 'Capacidade Total',
+        data: [100],
+        backgroundColor: '#0e1826a4',
+      }
+    ]
   },
   options: {
+    plugins: {
+      legend: {
+        display: true,
+      }
+      
+    },
     scales: {
+      x: {
+        display: false
+      },
+      y: {
+        display: false,
+        beginAtZero: true
+      }
+    },
+    indexAxis: 'y',
+    barPercentage: -5,
+    scales: {
+      x: {
+        display: 'none',
+        beginAtZero: true,
+        max: 100
+      },
       y: {
         beginAtZero: true
       }
--- site/public/monitro/assets/js/chartInertes.js
+++ site/public/monitro/assets/js/chartInertes.js
@@ -3,11 +3,12 @@ const maquinasInertes = document.getElementById('maquinasInertes');
 new Chart(maquinasInertes, {
   type: 'pie',
   data: {
-    labels: ['Red', 'Blue'],
+    labels: ['Ativos', 'Inertes'],
     datasets: [{
       label: "none",
-      data: [12, 19],
-      borderWidth: 1
+      data: [12, 3],
+      borderWidth: 1,
+      backgroundColor: ['#28c8ef', '0E1826']
     }]
   },
   options: {
