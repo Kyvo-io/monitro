d1ad2da3b9c7c3720322e9920d7c461166c5a3ba
RobertoTurbiani <roberto.tfariasjr@sptech.school>
2023-11-15 22:12:03 -0300

    ajuste na validação de alerta

--- site/public/monitro/assets/js/listarServidores.js
+++ site/public/monitro/assets/js/listarServidores.js
@@ -10,24 +10,20 @@ async function listarServidoresEmpresa() {
         var json = await buscar.json();
         listaServidores = json;
 
-
         for (var i = 0; i < listaServidores.length; i++) {
             var servidor = listaServidores[i];
-           
+                      
             nomeMaquina.innerHTML = `Máquina: ${servidor.nomeServidor}`;
             idMaquina.innerHTML = `${servidor.idServidor}`;
 
-            console.log(servidor.nivelAlerta)
-
-
-                  if (servidor.tipoServidor = 'Cloud Aws') {
+                  if (servidor.tipoServidor == 'Cloud AWS') {
             nomeMaquina.innerHTML += ` <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-amazon" viewBox="0 0 16 16">
             <path d="M10.813 11.968c.157.083.36.074.5-.05l.005.005a89.521 89.521 0 0 1 1.623-1.405c.173-.143.143-.372.006-.563l-.125-.17c-.345-.465-.673-.906-.673-1.791v-3.3l.001-.335c.008-1.265.014-2.421-.933-3.305C10.404.274 9.06 0 8.03 0 6.017 0 3.77.75 3.296 3.24c-.047.264.143.404.316.443l2.054.22c.19-.009.33-.196.366-.387.176-.857.896-1.271 1.703-1.271.435 0 .929.16 1.188.55.264.39.26.91.257 1.376v.432c-.199.022-.407.044-.621.065-1.113.114-2.397.246-3.36.67C3.873 5.91 2.94 7.08 2.94 8.798c0 2.2 1.387 3.298 3.168 3.298 1.506 0 2.328-.354 3.489-1.54l.167.246c.274.405.456.675 1.047 1.166ZM6.03 8.431C6.03 6.627 7.647 6.3 9.177 6.3v.57c.001.776.002 1.434-.396 2.133-.336.595-.87.961-1.465.961-.812 0-1.286-.619-1.286-1.533ZM.435 12.174c2.629 1.603 6.698 4.084 13.183.997.28-.116.475.078.199.431C13.538 13.96 11.312 16 7.57 16 3.832 16 .968 13.446.094 12.386c-.24-.275.036-.4.199-.299l.142.087Z"/>
             <path d="M13.828 11.943c.567-.07 1.468-.027 1.645.204.135.176-.004.966-.233 1.533-.23.563-.572.961-.762 1.115-.191.154-.333.094-.23-.137.105-.23.684-1.663.455-1.963-.213-.278-1.177-.177-1.625-.13l-.09.009c-.095.008-.172.017-.233.024-.193.021-.245.027-.274-.032-.074-.209.779-.556 1.347-.623Z"/>
           </svg>
           
           `
-        } else if (servidor.tipoServidor = 'Cloud Azure'){
+        } else if (servidor.tipoServidor == 'Cloud Azure'){
             nomeMaquina.innerHTML += `  
 
           <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 48 48">
@@ -47,12 +43,12 @@ async function listarServidoresEmpresa() {
    `
         }
 
-    if (servidor.nivelAlerta = 'alto') {
+    if (servidor.nivelAlerta == 'alto') {
         statusMaquina.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="red"class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
         <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
       </svg>
       `
-    } else if(servidor.nivelAlerta = 'medio'){
+    } else if(servidor.nivelAlerta == 'medio'){
         statusMaquina.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="orange"class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
         <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
       </svg>
@@ -73,5 +69,8 @@ async function listarServidoresEmpresa() {
         console.error("Erro ao listar servidores:", erro);
     }
 }
+
+
+
 
 listarServidoresEmpresa();
