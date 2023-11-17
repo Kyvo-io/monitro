ea00fe0bb26e40e2a8fa1e879a21c25bf257bfd9
RobertoTurbiani <roberto.tfariasjr@sptech.school>
2023-11-17 14:15:33 -0300

    ajuste cadastro servidor

--- site/src/models/servidorModel.js
+++ site/src/models/servidorModel.js
@@ -21,24 +21,24 @@ function listarServidoresEmpresa() {
 
 }
 
-function name(params) {
- var instrucao =  `SELECT nomeTipo FROM componente
-  JOIN tipoComponente ON fkTipoComponente = idTipoComponente
-  WHERE fkServidor = idServidor ;`
-  return database.executar(instrucao);
+// function name(params) {
+//  var instrucao =  `SELECT nomeTipo FROM componente
+//   JOIN tipoComponente ON fkTipoComponente = idTipoComponente
+//   WHERE fkServidor = idServidor ;`
+//   return database.executar(instrucao);
 
-}
+// }
 
 
-async function cadastrarServidor(logradouro,cep,bairro,numero,cidade,uf,sistemaOperacional,nomeServidor,fkEmpresa) {
+async function cadastrarServidor(logradouro,cep,bairro,numero,cidade,uf,idEndereco,sistemaOperacional,nomeServidor,fkEmpresa) {
 
   var idEndereco = await endereco.cadastrarEndereco(logradouro, cep, bairro, numero, cidade, uf)  
 
   try {
 
     var insertServidorQuery = `
-      INSERT INTO servidor (fkEndereco, sistemaOperacional, nomeServidor, fkEmpresa)
-      VALUES (${idEndereco}, '${sistemaOperacional}', '${nomeServidor}', ${fkEmpresa});
+      INSERT INTO servidor (fkEndereco, tipoServidor, nomeServidor, fkEmpresa)
+      VALUES (${idEndereco},'${sistemaOperacional}','${nomeServidor}', ${fkEmpresa});
     `;
 
     const servidorInsertResult = await database.executar(insertServidorQuery);
